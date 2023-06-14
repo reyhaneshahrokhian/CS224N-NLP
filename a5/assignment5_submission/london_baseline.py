18ea13806c6985652932af831e4d47421b2f4ca0
reyhane <reyhane.shahrokh@gmail.com>
2023-06-14 18:58:03 +0330

    assignment5 of cs224n

---
+++ a5/assignment5_submission/london_baseline.py
@@ -0,0 +1,8 @@
+# Calculate the accuracy of a baseline that simply predicts "London" for every
+#   example in the dev set.
+# Hint: Make use of existing code.
+# Your solution here should only be a few lines.
+import utils
+
+total, correct = utils.evaluate_places("birth_dev.tsv", ["London"] * 500)
+print('Correct: {} out of {}: {}%'.format(correct, total, correct / total * 100))
